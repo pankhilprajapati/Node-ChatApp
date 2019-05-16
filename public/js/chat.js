87c47117c4a4eaa47489dc3305f2607c2d5209a4
pankhilprajapati <pankhil.prajapati2017@vitstudent.ac.in>
2019-05-16 16:52:57 +0530

    current locations of the user

--- public/js/chat.js
+++ public/js/chat.js
@@ -12,3 +12,18 @@ document.querySelector('#message-form').addEventListener("submit", (e)=>{
 
     socket.emit('sendMessage', message)
 })
+
+
+document.querySelector('#send-location').addEventListener('click',()=>{
+    if(!navigator.geolocation){
+        return alert("Not supported by the browser")
+    }
+    
+    navigator.geolocation.getCurrentPosition((p)=>{
+        
+        socket.emit("sendLocation",{
+            latitude: p.coords.latitude,
+            longitude: p.coords.longitude
+        })
+    })
+})
