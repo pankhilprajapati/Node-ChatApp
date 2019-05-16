26eff1b28ce852f44a58e1b1bf1d926ae0ace85e
pankhilprajapati <pankhil.prajapati2017@vitstudent.ac.in>
2019-05-16 15:30:13 +0530

    socket.io events with basic broadcasting of message

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,12 @@
-io()
+const socket = io()
+
+socket.on("countUpdated",(c)=>{
+    console.log("The count is updated",c)
+})
+
+document.querySelector('#increment').addEventListener('click',()=>{
+    console.log('clicked')
+    socket.emit("increment")
+
+})
+
